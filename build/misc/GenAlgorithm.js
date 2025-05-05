8cecd3cc3171631aa80103c213ced0b9dc20cbe5
erjbroek <broe0136@hz.nl>
2025-05-05 12:18:22 +0200

    added ui as base for further changes

--- build/misc/GenAlgorithm.js
+++ build/misc/GenAlgorithm.js
@@ -20,18 +20,22 @@ export default class GenAlgorithm {
     update(deltaTime) {
     }
     renderTarget(canvas) {
-        CanvasUtil.fillRectangleWithGradient(canvas, canvas.width * 0.55, canvas.height * 0.03, canvas.width * 0.3, canvas.height * 0.94, [
-            { red: 25, green: 25, blue: 25, opacity: 1, stop: 0.1 },
-            { red: this.targetColor.r, green: this.targetColor.g, blue: this.targetColor.b, opacity: 1, stop: 0.2 }
-        ], 180);
+        CanvasUtil.fillRectangleWithGradient(canvas, canvas.width * 0.55, canvas.height * 0.03, canvas.width * 0.2, canvas.height * 0.94, [
+            { red: 25, green: 25, blue: 25, opacity: 1, stop: 0 },
+            { red: this.targetColor.r, green: this.targetColor.g, blue: this.targetColor.b, opacity: 1, stop: 0.1 }
+        ], 180, 10);
         const hexColor = `#${((1 << 24) + (this.targetColor.r << 16) + (this.targetColor.g << 8) + this.targetColor.b).toString(16).slice(1).toUpperCase()}`;
         const textColor = this.getContrastingColor(this.targetColor);
-        CanvasUtil.writeText(canvas, hexColor, canvas.width * 0.73, canvas.height * 0.5, 'center', 'system-ui', 50, textColor, 500);
-        CanvasUtil.drawRectangle(canvas, canvas.width * 0.03, canvas.height * 0.03, canvas.width * 0.82, canvas.height * 0.94, this.targetColor.r, this.targetColor.g, this.targetColor.b, 1, 4, 5);
+        CanvasUtil.writeText(canvas, hexColor, canvas.width * 0.65, canvas.height * 0.32, 'center', 'system-ui', 50, textColor, 500);
+        CanvasUtil.drawRectangle(canvas, canvas.width * 0.03, canvas.height * 0.03, canvas.width * 0.72, canvas.height * 0.94, this.targetColor.r, this.targetColor.g, this.targetColor.b, 1, 4, 10);
+        CanvasUtil.fillRectangle(canvas, canvas.width * 0.74, canvas.height * 0.03, canvas.width * 0.23, canvas.height * 0.94, this.targetColor.r, this.targetColor.g, this.targetColor.b, 1, 10);
+        CanvasUtil.drawRectangle(canvas, canvas.width * 0.74, canvas.height * 0.03, canvas.width * 0.23, canvas.height * 0.94, this.targetColor.r, this.targetColor.g, this.targetColor.b, 1, 4, 10);
+        CanvasUtil.fillRectangle(canvas, canvas.width * 0.76, canvas.height * 0.05, canvas.width * 0.2, canvas.height * 0.9, this.targetColor.r - 15, this.targetColor.g - 15, this.targetColor.b - 15, 1, 10);
+        CanvasUtil.fillRectangle(canvas, canvas.width * 0.58, canvas.height * 0.55, canvas.width * 0.22, canvas.height * 0.4, this.targetColor.r - 15, this.targetColor.g - 15, this.targetColor.b - 15, 1, 10);
     }
     getContrastingColor({ r, g, b }) {
         const brightness = r * 0.299 + g * 0.587 + b * 0.114;
-        const factor = brightness > 186 ? 0.7 : 1.3;
+        const factor = brightness > 186 ? 0.5 : 2;
         const newR = Math.min(255, Math.round(r * factor));
         const newG = Math.min(255, Math.round(g * factor));
         const newB = Math.min(255, Math.round(b * factor));
